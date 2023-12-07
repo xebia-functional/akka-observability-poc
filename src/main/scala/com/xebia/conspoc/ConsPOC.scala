4797b29a2a849bab3bb22f8724a4cd66b63fd8cd
yurianthedev <bryan.garzon@xebia.com>
2023-12-07 15:27:51 -0500

    Filter by console argument

--- src/main/scala/com/xebia/conspoc/ConsPOC.scala
+++ src/main/scala/com/xebia/conspoc/ConsPOC.scala
@@ -2,19 +2,40 @@ package com.xebia.conspoc
 
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
-import akka.kafka.{ConsumerSettings, Subscriptions}
+import akka.kafka.ConsumerSettings
+import akka.kafka.Subscriptions
 import akka.kafka.scaladsl.Consumer
-import akka.stream.scaladsl.{Keep, Sink}
+import akka.stream.scaladsl.Keep
+import akka.stream.scaladsl.Sink
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
 
 import scala.concurrent.Future
 import scala.io.StdIn
 
+sealed trait FilterInt {
+  def apply(x: Int): Boolean
+}
+
+object FilterInt {
+  final case object Even extends FilterInt {
+    def apply(x: Int): Boolean = x % 2 == 0
+  }
+
+  final case object Odd extends FilterInt {
+    def apply(x: Int): Boolean = !Even(x)
+  }
+}
+
 object ConsPOC extends App {
   implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "consumer")
 
-  val config = system.settings.config.getConfig("akka.kafka.consumer")
+  val filter = args(1) match {
+    case "even" => FilterInt.Even
+    case _      => FilterInt.Odd
+  }
+
+  val config           = system.settings.config.getConfig("akka.kafka.consumer")
   val bootstrapServers = system.settings.config.getString("bootstrapServers")
   val consumerSettings = ConsumerSettings(config, new StringDeserializer, new StringDeserializer)
     .withBootstrapServers(bootstrapServers)
@@ -24,6 +45,7 @@ object ConsPOC extends App {
   val (control, streamCompletion): (Consumer.Control, Future[akka.Done]) =
     Consumer
       .plainSource(consumerSettings, Subscriptions.topics("users"))
+      .filter(record => filter(record.value().toInt))
       .map(msg => println(s"Received: ${msg.value()}"))
       .toMat(Sink.ignore)(Keep.both)
       .run()
@@ -33,7 +55,9 @@ object ConsPOC extends App {
   StdIn.readLine()
 
   // Shutdown logic
-  control.shutdown().onComplete { _ =>
-    system.terminate()
-  }(system.executionContext)
+  control
+    .shutdown()
+    .onComplete { _ =>
+      system.terminate()
+    }(system.executionContext)
 }
