d1e57b4b36091d6ee69ad198b683df6e1a03935f
Rafa Paradela <rafa.paradela@xebia.com>
2023-12-06 22:59:29 -0500

    updates README.md

--- README.md
+++ README.md
@@ -18,3 +18,21 @@ Run the consumer:
 sbt "runMain com.xebia.conspoc.ConsPOC"
 ```
 
+Run the UserActor app:
+
+```bash
+sbt "runMain com.xebia.useractorpoc.UserApp"
+```
+
+And see the logs:
+
+```bash
+[INFO] [com.xebia.useractorpoc.UserActor$] - Got command Add(1,Actor[akka://UserActorPoC/deadLetters#0])
+[INFO] [com.xebia.useractorpoc.UserActor$] - Handling event Added(1) 
+```
+
+Run the UserActor tests:
+
+```bash
+sbt "testOnly com.xebia.useractorpoc.UserActorSpec" 
+```
--- src/main/scala/com/xebia/useractorpoc/UserApp.scala
+++ src/main/scala/com/xebia/useractorpoc/UserApp.scala
@@ -6,20 +6,8 @@ import akka.pattern.StatusReply
 
 object UserApp extends App {
 
-
   val system: ActorSystem[UserActor.Command] = ActorSystem(UserActor("1"), "UserActorPoC")
   val ref = system.deadLetters[StatusReply[UserActor.Summary]]
 
-  println("entro 33333")
   system ! UserActor.Add("1", ref)
-
-  println("ahora?")
-//  println("entro 33333")
-//  system ! UserActor.Add("2")
-//  println("entro 33333")
-//  system ! UserActor.Add("3")
-//  println("entro 33333")
-//  system ! UserActor.Add("4")
-
-
 }
