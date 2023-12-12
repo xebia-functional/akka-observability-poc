38a03fe489d77a5f087fc8562d5bfc2be81151eb
Bernardo Herrera <beherrera@nclcorp.com>
2023-12-11 19:59:49 -0500

    Fixed compile error for topicCreator.scala

--- src/main/scala/com/xebia/prodpoc/TopicCreator.scala
+++ src/main/scala/com/xebia/prodpoc/TopicCreator.scala
@@ -3,7 +3,7 @@ package com.xebia.prodpoc
 import org.apache.kafka.clients.admin.{AdminClient, AdminClientConfig, NewTopic}
 
 import java.util.Properties
-import scala.collection.JavaConverters._
+import scala.jdk.CollectionConverters._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
